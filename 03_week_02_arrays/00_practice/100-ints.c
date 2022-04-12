5292dcc6899f960c0769fb3216572a1dbd72515d
Julian Kohr <julian.kohr@kuka.com>
2022-04-12 12:04:49 +0200

    reworked practice assignments in week 02

--- 03_week_02_arrays/00_practice/100-ints.c
+++ 03_week_02_arrays/00_practice/100-ints.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// create initial array
 int ints[0];
+// prtotype of function fillarray
 void fillarray(int z);
 
+// initiate main function
 int main(void)
 {
+    // ask how many elements the array should have
     int x = get_int("How many elements to generate in the array? ");
+    // feed function fillarray with input from user
     fillarray(x);
+    // define variable y
     int y = 0;
+    // initialize while loop
     while (true)
     {
+        // ask user from which index element the value is needed
         y = get_int("From which index element do you want its value? ");
+        // check if index element is valid
         if (y >= 0 && y < x)
         {
-
+            // if valid, print the value inside the element
             printf("The value of element %i is %i\n", y, ints[y]);
         }
         else
         {
+            // else print invalid message
             printf("Invalid index element!\n");
             return 1;
         }
     }
 }
 
+// define fillarray function
 void fillarray(int z)
 {
+    // start for loop filling the requested amount of elements with the index number + 1, so value at index 0 will be 1 and so on
     for (int i = 0; i < z; i++)
     {
         ints[i] = i + 1;
--- 03_week_02_arrays/00_practice/triangle.c
+++ 03_week_02_arrays/00_practice/triangle.c
@@ -1,28 +1,37 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// prototype of function valid_triangle
 bool valid_triangle(float x, float y, float z);
 
+// initiate main function
 int main(void)
 {
+    // ask for lenghts of side x, y and z
     float x = get_float("X: ");
     float y = get_float("Y: ");
     float z = get_float("Z: ");
 
+    // feed function valid_triangle with user input and store the result (true || false) in boolean variable result
     bool result = valid_triangle(x, y, z);
 
+    // if valid_triangle returns true print valid triangle, if false print invalid traingle
     printf("%s\n", result ? "valid triangle" : "invalid triangle");
 }
 
+// define valid_triangle function
 bool valid_triangle(float x, float y, float z)
 {
+    // if any of the inputs is smaller or equal 0 return false
     if (x <= 0 || y <= 0 || z <= 0)
     {
         return false;
     }
+    // if any combinations of the lengths is smaller or equal to another length, return false
     if ((x + y <= z) || (x + z <= y) || (y + z <= x))
     {
         return false;
     }
+    // else return true
     return true;
 }
