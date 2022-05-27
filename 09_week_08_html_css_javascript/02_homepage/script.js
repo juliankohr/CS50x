58b94101764b11a003c848a51f630c9d9c4317d7
Julian Kohr <julian.kohr@kuka.com>
2022-05-27 17:26:36 +0200

    added homepage assignment

---
+++ 09_week_08_html_css_javascript/02_homepage/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let button = document.getElementById("up");
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          button.style.display = "block";
+        } else {
+          button.style.display = "none";
+        }
+      }
+});
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
